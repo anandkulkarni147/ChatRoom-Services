f265b5a8d4e897a05f629f87eb1eff70171c9fb2
Anand Kulkarni <kulkarnianand147@gmail.com>
2022-11-30 11:52:57 -0800

    build

---
+++ build/precache-manifest.6465d2c9b4258225d79230d710829bb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9624eaf94edc041288bb85b8137a2eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f80ff65203448c895061",
+    "url": "/static/css/main.4554b2fa.chunk.css"
+  },
+  {
+    "revision": "b96d595fe332a4efb68b",
+    "url": "/static/js/2.9ea8f9ee.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.9ea8f9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80ff65203448c895061",
+    "url": "/static/js/main.f931a708.chunk.js"
+  },
+  {
+    "revision": "cb120f972db6bf9fc458",
+    "url": "/static/js/runtime-main.4b3dce36.js"
+  }
+]);
